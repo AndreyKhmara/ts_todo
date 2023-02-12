92b0b2c46ae73bc7e92dcf6fc4f6174f12246feb
AndreyKhmara <kelevra101smu@gmail.com>
2023-02-12 23:25:25 +0300

    create redux types

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './components/App';
+import { Provider } from 'react-redux';
 
+import App from './components/App';
+
+import store from './store';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
